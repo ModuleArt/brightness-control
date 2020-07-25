0a826e10b7517595d393d622f395fe5647cbb66e
Eugene Volynko <40366303+Beelink@users.noreply.github.com>
2020-07-25 10:39:04 +0300

    Add files via upload

---
+++ brightness-lib/Controller.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Management;
+using System.Runtime.InteropServices;
+
+namespace brightness_lib
+{
+    public static class Controller
+    {
+        public static int Get()
+        {
+            var mclass = new ManagementClass("WmiMonitorBrightness")
+            {
+                Scope = new ManagementScope(@"\\.\root\wmi")
+            };
+            var instances = mclass.GetInstances();
+            foreach (ManagementObject instance in instances)
+            {
+                return (byte)instance.GetPropertyValue("CurrentBrightness");
+            }
+            return 0;
+        }
+
+        public static void Set(int brightness)
+        {
+            var mclass = new ManagementClass("WmiMonitorBrightnessMethods")
+            {
+                Scope = new ManagementScope(@"\\.\root\wmi")
+            };
+            var instances = mclass.GetInstances();
+            var args = new object[] { 1, brightness };
+            foreach (ManagementObject instance in instances)
+            {
+                instance.InvokeMethod("WmiSetBrightness", args);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    }
+}
